d95185da68d6389d298ed4768ae812c15cd56413
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2019-02-12 08:38:45 -0500

    Files used to test the functions in the shell program

---
+++ tests.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "parser.h"
+
+int main() {
+  char *input = read_input();
+  char **segments = parse_input(input);
+
+  int index = 0;
+  while (segments[index] != NULL) {
+    printf("[%d] %s\n", index, segments[index]);
+    index++;
+  }
+  
+  return 1;
+}
